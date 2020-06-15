932e56402996d727c22d884e71d424880e5cc455
Cule219 <culaficstefan@gmail.com>
2020-06-15 17:14:52 -0400

    second iteration

--- src/viking.js
+++ src/viking.js
@@ -1,21 +1,38 @@
 // Soldier
 class Soldier {
-    constructor(health, strength){
-        this.health = health;
-        this.strength = strength
-    }
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
 
-    attack = () => {
-        return this.strength;
-    }
+  attack = () => {
+    return this.strength;
+  };
 
-    receiveDamage = (theDamage) => {
-        this.health -= theDamage;
-    }
+  receiveDamage = (theDamage) => {
+    this.health -= theDamage;
+  };
 }
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  }
+
+  receiveDamage = (theDamage) => {
+    this.health -= theDamage;
+    if (this.health > 0) {
+      return `${this.name} has received ${theDamage} points of damage`;
+    } else {
+      return `${this.name} has died in act of combat`;
+    }
+  };
+  battleCry = () => {
+    return "Odin Owns You All!";
+  };
+}
 
 // Saxon
 class Saxon {}
